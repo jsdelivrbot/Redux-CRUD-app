7a40434a0c92d3b3ba6a7b9324f354662b3a0abc
Nguyen Manh Tung <nguyenmanhtung848@gmail.com>
2017-02-19 20:42:48 +0700

    inlcude redux promise react-router

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostIndex from './components/post_index';
 
 const Greeting = () => {
     return <div>Hey there</div>
 };
+
 export default (
     <Route path="/" component={App} >
-        <Route path="greet" component={Greeting} />
-        <Route path="greet2" component={Greeting} />
-        <Route path="greet3" component={Greeting} />
+        <IndexRoute component={PostIndex}/>
     </Route>
 )
